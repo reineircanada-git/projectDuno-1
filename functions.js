f66f99879b821ec61af5ff10edc892d207cc18b3
reineircanada-git <113652226+reineircanada-git@users.noreply.github.com>
2022-09-16 10:10:19 +0800

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,82 @@
+//var socket = io()
+$(() => {
+    $("#send").click(() => {
+        var message = { name: $("#name").val(), message: $("#message").val(), dateAdded: $("#dateToday").val(), motType: $("input[name='rdbtn']:checked").val()  }
+        postMessages(message)
+        $("#name").val('')
+        $("#name").prop('disabled', true)
+        $("#message").val('')
+        $("#message").prop('disabled', true)
+        $("input[name='rdbtn']").prop('checked', false)
+        $('#alert').empty().show().html("Message Sent!").delay(3000).fadeOut(300);
+    })
+    getMessages()
+})
+
+//socket.on('message', addMessages)
+
+function changePlaceholder() {
+    $("#name").prop('disabled', false)
+    $("#name").prop('required', true)
+    $("#message").prop('disabled', false)
+    if ($("input[name='rdbtn']:checked").val() == 'Song') {
+        $("#message").attr('placeholder', 'Paste a Link or Type the Title of the Song')
+    }
+    else if ($("input[name='rdbtn']:checked").val() == 'Message') {
+        $("#message").attr('placeholder', 'Type your message here')
+    }
+    else {
+        $("#message").attr('placeholder', 'What do you want to do?')
+    }
+}
+
+function addMessages(message) {
+    var date = new Date(message.dateAdded),
+        month = date.toLocaleString('default',{month: 'long'}),
+        day = '' + date.getDate(),
+        year = date.getFullYear();
+
+    //if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    date = month + ' ' + day + ', ' + year
+
+    var motivType = message.motType
+    var src = ""
+    if(motivType == "Message"){
+        src = "./icons/letter.png"
+    }
+    else if (motivType == "Task"){
+        src = "./icons/task.png"
+    }
+    else if (motivType == "Song"){
+        src = "./icons/music.png"
+    }
+
+    $("#messages").append(`
+        <div class="container">
+            <div class="column">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <img src="${src}" style="width:40px">
+                        <p class="card-text">
+                            From: ${message.name} <br>
+                            ${message.message} <br>
+                        </p>
+                    </div>   
+                </div>
+                <br>
+            </div>
+        </div> 
+    `);
+}//appends messages to messages div
+
+function getMessages() {
+    $.get('http://localhost:3001/messages', (data) => {
+        data.forEach(addMessages);
+    })
+}
+
+function postMessages(message) {
+    $.post('http://localhost:3001/messages', message)
+}
---
+++ server.js
@@ -0,0 +1,55 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+const { Socket } = require('dgram')
+var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+var mongoose = require('mongoose')
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+var dbUrl = 'mongodb+srv://reineir:reineir@cluster0.jkbtl20.mongodb.net/test'
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String,
+    motType: String,
+    dateAdded: {type: Date, default: Date.now}
+})
+
+//gets all the messages from the DB
+app.get('/messages', (req, res) => {
+    Message.find({motType: { $ne: null }},(err, message) => {
+        res.send(message)
+    }) 
+
+})
+
+//Saves a message to the DB
+app.post('/messages', (req, res) => {
+    var messages = new Message (req.body)
+
+    messages.save((err) =>{
+        if(err){
+            sendStatus(500)
+        }
+
+        io.emit('message', req.body)
+        res.sendStatus(200)
+    })
+    
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+})
+
+mongoose.connect(dbUrl, (err) => {
+    console.log('mongo db connection', err)
+})
+
+var server = http.listen(3001, () => {
+    console.log('server is listening on port ', server.address().port)
+})
