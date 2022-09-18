c80b36876aae02139fc888ee4bb8250835fd4637
reineircanada-git <113652226+reineircanada-git@users.noreply.github.com>
2022-09-18 15:32:34 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 var mongoose = require('mongoose')
+var cors = require('cors')
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(cors())
 
 var dbUrl = 'mongodb+srv://reineir:reineir@cluster0.jkbtl20.mongodb.net/test'
 
